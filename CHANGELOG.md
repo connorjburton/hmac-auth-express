b992ff8ee8abcd7428ac8d0ca5c1d2abd09de4d1
Connor Burton <Connor.burton@warwickicsystems.com>
2018-12-12 11:13:09 +0000

    Adding new validation rules, moving validation to its own file and adding comments

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [1.0.2] - 2018-12-12
+### Changed
+- Moved validation to it's own `validateArguments.js` file
+- Added validaton for `options.maxInterval` and `options.error`
+- Added comments to `index.js`
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const stringToBuffer = require('./stringToBuffer');
+const validateArguments = require('./validateArguments');
 
 module.exports = function(secret, options = {}) {
     options.algorithm = options.algorithm || 'sha256';
@@ -8,35 +9,27 @@ module.exports = function(secret, options = {}) {
     options.maxInterval = options.maxInterval || 60 * 5;
     options.error = options.error || 'Invalid request';
 
-    if (!secret || typeof secret !== 'string') {
-        throw new TypeError(`Invalid value provided for property secret. Expected non-empty string but got '${secret}' (type: ${typeof secret})`);
-    }
-
-    if (!crypto.getHashes().includes(options.algorithm)) {
-        throw new TypeError(`Invalid value provided for property options.algorithm. Expected value from crypto.getHashes() but got ${options.algorithm}`);
-    }
-
-    if (!options.identifier || typeof options.identifier !== 'string') {
-        throw new TypeError(`Invalid value provided for property options.identifier. Expected non-empty string but got '${options.identifier}' (type: ${typeof options.identifier})`);
-    }
-
-    if (!options.header || typeof options.header !== 'string') {
-        throw new TypeError(`Invalid value provided for property options.header. Expected non-empty string but got '${options.header}' (type: ${typeof options.header})`);
+    const error = validateArguments(secret, options);
+    if (error) {
+        throw error;
     }
 
     return function(request, response, next) {
         const hmac = crypto.createHmac(options.algorithm, secret);
 
+        // checked the header we specified exists in request headers
         if (!(options.header in request.headers)) {
             console.error(`Header provided not in sent headers. Expected ${options.header} but not found in request.headers`);
             return response.status(401).send(options.error);
         }
 
+        // check the header we specified has the identifier we specified
         if (!request.headers[options.header].startsWith(options.identifier)) {
             console.error(`Header did not start with correct identifier. Expected ${options.identifier} but not found in options.header`);
             return response.status(401).send(options.error);
         }
 
+        // is the unix timestamp present in the header
         const unixRegex = /(\d{1,10}):/;
         const unixMatch = unixRegex.exec(request.headers[options.header]);
         if (!unixMatch || unixMatch.length !== 2) {
@@ -44,12 +37,14 @@ module.exports = function(secret, options = {}) {
             return response.status(401).send(options.error);
         }
 
+        // is the unix timestamp difference to current timestamp larger than maxInterval
         const timeDiff = Math.floor(Date.now() / 1000) - parseInt(unixMatch[1]);
         if (timeDiff > options.maxInterval || timeDiff < 0) {
             console.error('Time difference between generated and requested time is too great');
             return response.status(401).send(options.error);
         }
 
+        // check HMAC digest exists in header
         const hashRegex = /:(.*$)/;
         const hashMatch = hashRegex.exec(request.headers[options.header]);
         if (hashMatch.length !== 2) {
@@ -57,17 +52,20 @@ module.exports = function(secret, options = {}) {
             return response.status(401).send(options.error);
         }
 
-        hmac.update(request.method);
-        hmac.update(request.baseUrl);
+        hmac.update(request.method); // add verb e.g POST, GET
+        hmac.update(request.baseUrl); // add url e.g /api/order
 
+        // if we have a request body, create a md5 hash of it and add it to the hmac
         if (Object.keys(request.body).length) {
             const hash = crypto.createHash('md5');
-            hash.update(JSON.stringify(request.body));
+            hash.update(JSON.stringify(request.body)); // we add it as a json string
             hmac.update(hash.digest('hex'));
         }
 
-        const hmacDigest = hmac.digest();
+        const hmacDigest = hmac.digest(); // returns Uint8Array buffer
         const sourceDigest = stringToBuffer(hashMatch[1]); // convert string to buffer
+
+        // use timing safe check to prevent timing attacks
         if (hmacDigest.length !== sourceDigest.length || !crypto.timingSafeEqual(hmacDigest, sourceDigest)) {
             console.error('HMAC\'s did not match');
             return response.status(401).send(options.error);
