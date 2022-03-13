8405601e06f6371236c8006fb0815b3b9be4b898
Connor Burton <me@connorburton.com>
2022-03-13 17:57:31 +0000

    style: prettier styling + gitattributes for line endings

--- src/order.ts
+++ src/order.ts
@@ -8,7 +8,7 @@ export default function order(o: UnknownObject): UnknownObject {
     // get the keys in lexigraphic order
     // we dont care about `a === b` as you can't have two keys of same
     // name within an object
-    const ks = Object.keys(o).sort((a, b) => a > b ? 1 : -1);
+    const ks = Object.keys(o).sort((a, b) => (a > b ? 1 : -1));
 
     return ks.reduce((n: UnknownObject, k: string) => {
         n[k] = order(o[k] as UnknownObject);
